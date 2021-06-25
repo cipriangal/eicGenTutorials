a2674db12efadd1356834ddae349648fc2520fd0
Barak Schmookler <bschmookler1@gmail.com>
2021-06-24 20:33:59 -0400

    Update for SDCC

---
+++ SDCC_setup.csh
@@ -0,0 +1,7 @@
+#!/bin/csh
+ 
+setenv EIC_LEVEL dev
+source /cvmfs/eic.opensciencegrid.org/x8664_sl7/MCEG/releases/etc/eic_cshrc.csh
+setenv ROOT_INCLUDE_PATH ${EICDIRECTORY}/include/eicsmear:$ROOT_INCLUDE_PATH
+
+
---
+++ SDCC_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export EIC_LEVEL="dev"
+source /cvmfs/eic.opensciencegrid.org/x8664_sl7/MCEG/releases/etc/eic_bash.sh
+export ROOT_INCLUDE_PATH=${EICDIRECTORY}/include/eicsmear:$ROOT_INCLUDE_PATH
+
+
+
